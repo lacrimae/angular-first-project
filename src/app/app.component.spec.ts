2a004dfc87bbb666f4b02fb145e555978091b2d4
Kseniia_Lebedeva <kseniia_lebedeva@epam.com>
2022-09-30 16:36:29 +0200

    added FormsModule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-first-project'`, () => {
+  it(`should have as name 'Kseniia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-first-project');
+    expect(app.name).toEqual('Kseniia');
   });
 
   it('should render title', () => {
