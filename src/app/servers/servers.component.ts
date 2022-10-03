63a32398563abb2471be2e02c390d50037bbd733
Kseniia_Lebedeva <kseniia_lebedeva@epam.com>
2022-10-03 13:36:20 +0200

    added several components as practice using 'ng g c <name>' command

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  /** select by component*/
+  // selector: 'app-servers',
+  /** select by div*/
+  // selector: '[app-servers]',
+  /** select by class*/
+  selector: '.app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>`
